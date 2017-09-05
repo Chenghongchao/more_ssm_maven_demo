893172ec94eac1353288ff1e4a105574cc2b6ab4
chenghongchao <206286062@qq.com>
2017-09-05 14:54:28 +0800

    添加数据库

--- mytest-web/src/main/java/com/songci/mytest_one/controller/StudentController.java
+++ mytest-web/src/main/java/com/songci/mytest_one/controller/StudentController.java
@@ -3,12 +3,15 @@ package com.songci.mytest_one.controller;
 import com.songci.mytest_one.model.Student;
 import com.songci.mytest_one.model.utils.ResultVo;
 import com.songci.mytest_one.service.StudentService;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * Created by songl on 2017/8/10.
@@ -26,5 +29,11 @@ public class StudentController {
         if ("0".equals(id)){student=null;}
         else {student.setId(new Integer(id));}
         return studentService.findAllStudent(student);
+    }
+    @RequestMapping(value = "userName.json",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map getUserName(@RequestParam Map<String,Object> maps){
+
+        return maps;
     }
 }
